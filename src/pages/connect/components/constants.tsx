3c560d3f3463eff0bc80f236c09e7753140a644b
n3rdy.me <git@n3rdy.me>
2022-03-15 18:53:13 -0500

    Updated default query

--- src/pages/connect/components/constants.tsx
+++ src/pages/connect/components/constants.tsx
@@ -11,10 +11,10 @@ export type QueryViewType = "none" | "run" | "inspect" | "help";
 export type ResultViewType = "table" | "json" | "inspect";
 
 export const DefaultQuery = `/* Enter your sql query here */
+SELECT * FROM Makes ORDER BY Make_ID LIMIT 10
+;
 SELECT "Make_Name" as Make, "Model_Name" as Model
-  FROM Models
-  WHERE Model_Name >= 'FIREBLADE' 
-  ORDER BY Model_Name
-  LIMIT 10 OFFSET 1
+  FROM Models WHERE Model_Name >= 'FIREBLADE' 
+  ORDER BY Model_Name LIMIT 10 OFFSET 1
 ;
 `;
